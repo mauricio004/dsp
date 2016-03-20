6adb1845461b2cd21cb8617a9247795da3951191
Mauricio A Flores <mauricio@Mauricios-MacBook-Pro.local>
2016-03-20 14:00:40 -0700

    completed learn python 5, 6, 7, 8

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 # Hint:  use Google to find python function
 
 ####a) 
@@ -10,4 +12,20 @@ date_stop = '05282015'
 
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+
+a_a = date(2013, 01, 02)
+a_b = date(2015, 07, 28)
+delta = a_b - a_a
+print 'a = ', delta.days, 'days'
+
+a_a = date(2013, 12, 31)
+a_b = date(2015, 05, 28)
+delta = a_b - a_a
+print 'b = ', delta.days, 'days'
+
+a_a = date(1994, 01, 15)
+a_b = date(2015, 07, 14)
+delta = a_b - a_a
+print 'b = ', delta.days, 'days'
--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -3,56 +3,33 @@
 # The columns labeled 'Goals' and 'Goals Allowed' contain the total number of
 # goals scored for and against each team in that season (so Arsenal scored 79 goals
 # against opponents, and had 36 goals scored against them). Write a program to read the file,
-# then print the name of the team with the smallest difference in \‘for\’ and ‘against’ goals.
+# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
-
+# Team,Games,Wins,Losses,Draws,Goals,Goals Allowed,Points
 import csv
 
-# Team,Games,Wins,Losses,Draws,Goals,Goals Allowed,Points
 
-
-def get_team_with_smallest_diff_team(data):
+def get_team_with_smallest_diff(data):
     smallest_difference = None
     team = None
-
 
     try:
         with open(data, 'rb') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                team_difference = int(row['Goals']) - int(row['Goals Allowed'])
-
+                team_difference = abs(int(row['Goals']) - int(row['Goals Allowed']))
+                if team_difference < smallest_difference or smallest_difference is None:
+                    smallest_difference = team_difference
+                    team = row['Team']
     except Exception as err:
         print 'Cannot read file: ' + str(err)
 
+    return team
 
 
-def read_data(data):
-    dict_teams = {}
-    try:
-        with open(data, 'rb') as f:
-            reader = csv.DictReader(f, delimiter=',')
-
-            for row in reader:
-                dict_teams[row['Team']] = row
-    except Exception as err:
-        print 'Cannot read file: ' + str(err)
-
-    return dict_teams
 
 
-def get_min_score_difference(parsed_data):
-    # COMPLETE THIS FUNCTION
-    return
 
 
-def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
-    return
-
-
-filename = '/Users/Mauricio/ds/metis/prework/dsp/python/football.csv'
-# print get_min_score_difference(read_data(filename))
-get_team_with_smallest_diff_team(filename)
